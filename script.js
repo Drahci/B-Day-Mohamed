8a80cd8ec880d354a6a15e1b833ab4e905093d53
Richard <richard.ricciard@gmail.com>
2024-09-03 21:57:44 -0300

    feat: DataBase created

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const airtableApiKey =
+  "patGv2e7yAIyTsE1G.4d9956070746b0a30bf831b7c7616ff9201b3372beba64e252d676fc9ca8f27c";
+const baseId = "appcvpymHXLIWvQw3";
+const tableName = "guests";
 const btnConfirmar = document.querySelector("#confirm-button");
 const infoCard = document.querySelector("#info-card");
 const form1 = document.querySelector("#form1");
@@ -48,7 +52,7 @@ checkNao.addEventListener("change", (event) => {
   }
 });
 
-form2.addEventListener("submit", (event) => {
+form2.addEventListener("submit", async (event) => {
   event.preventDefault();
 
   if (!checkSim.checked && !checkNao.checked) {
@@ -62,9 +66,13 @@ form2.addEventListener("submit", (event) => {
     return;
   }
 
-  const accompanyNumber = parseInt(accompanyNumberInput.value, 10);
+  const name = document.querySelector("#name").value.trim();
+  const accompany = checkSim.checked;
+  const accompanyNumber = accompany
+    ? parseInt(accompanyNumberInput.value, 10)
+    : 0;
 
-  if (checkSim.checked && accompanyNumber > 5) {
+  if (accompany && accompanyNumber > 5) {
     form2.classList.add("validate-error");
     errorMessage.classList.remove("hidden");
     form2.addEventListener("animationend", (event) => {
@@ -74,7 +82,31 @@ form2.addEventListener("submit", (event) => {
     });
   } else {
     errorMessage.classList.add("hidden");
-    alert("Conto com a sua presença!");
+
+    try {
+      const response = await axios.post(
+        `https://api.airtable.com/v0/${baseId}/${tableName}`,
+        {
+          fields: {
+            name: name,
+            accompany: accompany,
+            accompany_number: accompanyNumber,
+          },
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${airtableApiKey}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      console.log("Dados enviados com sucesso:", response.data);
+      alert("Conto com a sua presença!");
+    } catch (error) {
+      console.error("Erro ao enviar os dados:", error);
+      alert("Houve um erro ao enviar os dados. Por favor, tente novamente.");
+    }
   }
 });
 
